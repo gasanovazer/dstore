2a4ecb0bf663beb6164f7a5874ebcc90f56d1fd5
x0gae <gasanovazer@outlook.com>
2023-10-31 17:07:18 +0400

    Доп поле для USER и авторизация

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,4 @@
-"""store URL Configuration
+﻿"""store URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -13,20 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from django.conf.urls.static import static
-from django.conf import settings
+from django.urls import path
 
-from products.views import index 
+from users.views import login, registration
 
+app_name = 'users'
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', index, name='index'),
-    path('products/', include('products.urls', namespace='products')),
-    path('users/', include('users.urls', namespace='users')),
-    
+    path('login/', login, name='login'),
+    path('registration/', registration, name='registration'),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
